27f8cd4da31c179dc5be122ce2db84bf76cce95c
SergeyFedoruk <7282289@tut.by>
2023-04-30 13:16:05 +0300

    add: keys array to  script.js & some style to style.css

--- script.js
+++ script.js
@@ -1,40 +1,78 @@
-let keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 92, 13, 92, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
+const keysColl = [
+    [
+        { code: 'Back-quote', value: '`',valueShift: '~', valueBel: 'ё', valueUppBel: 'Ё' },
+        { code: 'Digit1', value: 1, valueShift: '!' },
+        { code: 'Digit2', value: 2, valueShift: '@', valueShiftBel: '"'  },
+        { code: 'Digit3', value: 3, valueShift: '#', valueShiftBel: '№'  },
+        { code: 'Digit4', value: 4, valueShift: '$', valueShiftBel: ';'  },
+        { code: 'Digit5', value: 5, valueShift: '%' },
+        { code: 'Digit6', value: 6, valueShift: '^', valueShiftBel: ':'  },
+        { code: 'Digit7', value: 7, valueShift: '&', valueShiftBel: '?'  },
+        { code: 'Digit8', value: 8, valueShift: '*' },
+        { code: 'Digit9', value: 9, valueShift: '(' },
+        { code: 'Digit0', value: 0, valueShift: ')' },
+        { code: 'Minus', value: '-', valueShift: '_' },
+        { code: 'Equal', value: '=', valueShift: '+' },
+        { code: 'Backspace', value: 'Backspace', class: 'keyboard-key-up' },
+    ],
+    [
+        { code: 'Tab', value: 'Tab', class: 'keyboard-key-up' },
+        { code: 'KeyQ', value: 'q', valueUpp: 'Q', valueBel: 'й', valueUppBel: 'Й'  },
+        { code: 'KeyW', value: 'w', valueUpp: 'W', valueBel: 'ц', valueUppBel: 'Ц'  },
+        { code: 'KeyE', value: 'e', valueUpp: 'E', valueBel: 'у', valueUppBel: 'У'  },
+        { code: 'KeyR', value: 'r', valueUpp: 'R', valueBel: 'к', valueUppBel: 'К'  },
+        { code: 'KeyT', value: 't', valueUpp: 'T', valueBel: 'е', valueUppBel: 'Е'  },
+        { code: 'KeyY', value: 'y', valueUpp: 'Y', valueBel: 'н', valueUppBel: 'Н'  },
+        { code: 'KeyU', value: 'u', valueUpp: 'U', valueBel: 'г', valueUppBel: 'Г'  },
+        { code: 'KeyI', value: 'i', valueUpp: 'I', valueBel: 'ш', valueUppBel: 'Ш'  },
+        { code: 'KeyO', value: 'o', valueUpp: 'O', valueBel: 'ў', valueUppBel: 'Ў'  },
+        { code: 'KeyP', value: 'p', valueUpp: 'P', valueBel: 'з', valueUppBel: 'З'  },
+        { code: 'BracketLeft', value: '[', valueShift: '{', valueBel: 'х', valueUppBel: 'Х'   },
+        { code: 'BracketRight', value: ']', valueShift: '}', valueBel: '’', valueUppBel: '’'  },
+        { code: 'Backslash', value: '\\', valueShift: '|', valueShiftBel: '/'     },
+        { code: 'Delete', value: 'Del', class: 'keyboard-key-up' },
+    ],
+    [
+        { code: 'CapsLock', value: 'CapsLock', class: 'keyboard-key-up' },
+        { code: 'KeyA', value: 'a', valueUpp: 'A', valueBel: 'ф', valueUppBel: 'Ф'  },
+        { code: 'KeyS', value: 's', valueUpp: 'S', valueBel: 'ы', valueUppBel: 'Ы'  },
+        { code: 'KeyD', value: 'd', valueUpp: 'D', valueBel: 'в', valueUppBel: 'В'  },
+        { code: 'KeyF', value: 'f', valueUpp: 'F', valueBel: 'а', valueUppBel: 'А'  },
+        { code: 'KeyG', value: 'g', valueUpp: 'G', valueBel: 'п', valueUppBel: 'П'  },
+        { code: 'KeyH', value: 'h', valueUpp: 'H', valueBel: 'р', valueUppBel: 'Р'  },
+        { code: 'KeyJ', value: 'j', valueUpp: 'J', valueBel: 'о', valueUppBel: 'О'  },
+        { code: 'KeyK', value: 'k', valueUpp: 'K', valueBel: 'л', valueUppBel: 'Л'  },
+        { code: 'KeyL', value: 'l', valueUpp: 'L', valueBel: 'д', valueUppBel: 'Д'  },
+        { code: 'Semicolon', value: ';', valueShift: ':', valueBel: 'ж', valueUppBel: 'Ж'   },
+        { code: 'Quote', value: "'", valueShift: '"', valueBel: 'э', valueUppBel: 'Э'     },
+        { code: 'Enter', value: 'Enter', class: 'keyboard-key-up' },
+    ],
+    [
+        { code: 'ShiftLeft', value: 'Shift', class: 'keyboard-key-up' },
+        { code: 'Backslash', value: '\\', valueShift: '|', valueShiftBel: '/'   },
+        { code: 'KeyZ', value: 'z', valueUpp: 'Z', valueBel: 'я', valueUppBel: 'Я'    },
+        { code: 'KeyX', value: 'x', valueUpp: 'X', valueBel: 'ч', valueUppBel: 'Ч'    },
+        { code: 'KeyC', value: 'c', valueUpp: 'C', valueBel: 'с', valueUppBel: 'С'    },
+        { code: 'KeyV', value: 'v', valueUpp: 'V', valueBel: 'м', valueUppBel: 'М'   },
+        { code: 'KeyB', value: 'b', valueUpp: 'B', valueBel: 'і', valueUppBel: 'І'   },
+        { code: 'KeyN', value: 'n', valueUpp: 'N', valueBel: 'т', valueUppBel: 'Т'   },
+        { code: 'KeyM', value: 'm', valueUpp: 'M', valueBel: 'ь', valueUppBel: 'Ь'   },
+        { code: 'Comma', value: ',', valueShift: '<', valueBel: 'б', valueUppBel: 'Б'  },
+        { code: 'Period', value: '.', valueShift: '>', valueBel: 'ю', valueUppBel: 'Ю' },
+        { code: 'Slash', value: '/', valueShift: '?', valueBel: '.', valueUppBel: '.', valueShiftBel: ','  },
+        { code: 'ArrowUp', value: '↑' },
+        { code: 'ShiftRight', value: 'Shift', class: 'keyboard-key-up', },
+    ],
+    [
+        { code: 'ControlLeft', value: 'Ctrl', class: 'keyboard-key-up' },
+        { code: 'MetaRight', value: 'Win' },
+        { code: 'AltLeft', value: 'Alt' },
+        { code: 'Space', value: ' ', class: 'keyboard-key-space' },
+        { code: 'AltRight', value: 'Alt' },
+        { code: 'ControlRight', value: 'Ctrl', class: 'keyboard-key-up' },
+        { code: 'ArrowLeft', value: '←' },
+        { code: 'ArrowDown', value: '↓' },
+        { code: 'ArrowRight', value: '→' },
 
-//document.onkeypress = function(event){
-//   console.log(event);
-//    keyboard.push(event.charCode);
-   // console.log(keyboard);
-//}
-
-function init(){
-    let out = '';
-    for (let i=0; i<keyboard.length; i++){
-        if (i === 13 || i===25 || i===37 || i === 51){
-            out +='<div class="clearfix"></div>';
-        }
-        out +='<div class="k-key" data="'+keyboard[i] +'">'+String.fromCharCode(keyboard[i])+'</div>';
-    }
-    document.querySelector('#keyboard').innerHTML = out;
-}
-
-init();
-
-document.onkeypress = function (event){
-   console.log(event.code);
-    console.log(event.keyCode);
-    document.querySelectorAll('#keyboard .k-key').forEach(function(element){
-        element.classList.remove('active');
-    });
-    document.querySelector('#keyboard .k-key[data="'+event.keyCode+'"]').classList.add('active');
-}
-
-document.querySelectorAll('#keyboard .k-key').forEach(function(element){
-    element.onclick = function(event){
-        document.querySelectorAll('#keyboard .k-key').forEach(function(element) {
-            element.classList.remove('active');
-        });
-        let code = this.getAttribute('data');
-        this.classList.add('active')
-        console.log(code);
-    }
-}  )
+    ],
+];
